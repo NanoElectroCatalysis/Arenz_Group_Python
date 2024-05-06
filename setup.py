e43583e0ae5464e610fa9016e249770b108f063d
Gustav Wiberg <52791556+Guswib@users.noreply.github.com>
2024-05-06 21:55:22 +0200

    First

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Arenz_Group_Python',
+    version='0.0.1',
+    description='a pip-installable package with function for Arenz',
+    license='MIT',
+    packages=['ArenzGroupPython'],
+    author='Gustav Wiber',
+    author_email='gustav.wiberg@unibe.ch',
+    keywords=['example'],
+    url='https://github.com/guswib/arenz_group_python'
+)
