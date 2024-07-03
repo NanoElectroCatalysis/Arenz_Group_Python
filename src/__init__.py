64f7a756b3c879820d8323bc565440e2f5c680a6
Gustav Wiberg <gustav.wiberg@gmail.com>
2024-07-03 16:41:10 +0200

    a

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+# Leave it empty. This is just a special file that tells pip that your main module is in this folder. 
+# No need to add anything here. Feel free to delete this line when you make your own package.
+
+from Lib1._Class1
