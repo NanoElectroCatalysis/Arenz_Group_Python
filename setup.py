2c1883844f0707c080ae2a9acfc8c4a60416b4d1
Gustav Wiberg <gustav.wiberg@gmail.com>
2024-07-05 16:55:56 +0200

    updates

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.4' 
+VERSION = '0.0.5' 
 DESCRIPTION = 'a pip-installable package with function for Arenz Group'
 LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
 
