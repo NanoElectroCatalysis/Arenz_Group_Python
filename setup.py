2ed88ee0698f810f8594ee15fcba654ec85ff65e
Gustav Wiberg <52791556+Guswib@users.noreply.github.com>
2024-07-11 17:04:15 +0200

    f

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.10' 
+VERSION = '0.0.11' 
 DESCRIPTION = 'a pip-installable package with function for Arenz Group'
 LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
 
