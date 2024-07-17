13b4141569704e8e2afd1cfa9208f3092f776c03
Gustav Wiberg <gustav.wiberg@gmail.com>
2024-07-17 10:36:57 +0200

    cleaned up plots

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.15' 
+VERSION = '0.0.16' 
 DESCRIPTION = 'a pip-installable package with function for Arenz Group'
 LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
 
